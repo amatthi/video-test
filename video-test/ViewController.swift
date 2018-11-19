7505c59fec0a8749b21f7d2caa8d4e47e6a2a084
user912215 <user912215@tx005.local>
2018-11-19 12:06:12 -0600

    Initial Commit

---
+++ video-test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  video-test
+//
+//  Created by user912215 on 11/19/18.
+//  Copyright © 2018 user912215. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
